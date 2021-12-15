ff139bc7091973f6d4a110d0c7be9f6b9e626c77
Moon Bui <buinm@miamioh.edu>
2021-12-15 13:12:29 -0500

    front-end form for contact info (not functioning) and update projects section

--- typescript.js
+++ typescript.js
@@ -1,10 +1,15 @@
 // Code for fade in while scrolling
 $(document).ready(function() {
-    $('.fade-onload').each(function(i) {
-        $(this).animate({ 'opacity': '1', 'margin-top': '0px' }, 500);
+    $('.fade-onload-down').each(function(i) {
+        $(this).animate({ 'opacity': '1', 'margin-top': '0px' }, 600);
     });
+
+    // $('.fade-onload-up').each(function() {
+    //     $this.animate({ 'opacity': '1', 'margin-bottom': '0px' }, 500);
+    // });
+
     $(window).scroll(function() {
-        $('.fade-in').each(function(i) {
+        $('.fade-in-down').each(function(i) {
             var bottom_of_element = $(this).offset().top +
                 $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() +
@@ -16,3 +21,27 @@ $(document).ready(function() {
         });
     });
 });
+
+
+// Code to add code for model
+var modelUI = {};
+
+
+// Code for display form in modal box
+var modal = document.getElementById("myModal");
+var linkModal = document.getElementById("box_modal");
+var spanX = document.getElementsByClassName("close")[0];
+
+linkModal.onclick = function() {
+    modal.style.display = "block";
+}
+
+spanX.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
